b74ea6a1ba1f2006a8e7e6030d07bbf71b44d2b7
chaitali-1105 <chaitali.work11@gmail.com>
2026-06-15 12:20:19 +0530

    Time: 2 ms (88.52%), Space: 48.1 MB (22.98%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int far=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(i>far){
+                return false;
+            }
+            far=Math.max(far,i+nums[i]);
+            if(far>=n-1){
+                return true;
+            }
+        }
+        return true;
+    }
+}
