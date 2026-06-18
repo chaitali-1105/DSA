b3a46138b06c92ead702e06ebe0953d6329a3738
chaitali-1105 <chaitali.work11@gmail.com>
2026-06-18 14:16:19 +0530

    Time: 5 ms (79.13%), Space: 46 MB (53.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -13,3 +13,24 @@ class Solution {
         return original==reverse;
     }
 }
+
+//convert to string and then check by reversing 
+// class Solution {
+//     public boolean isPalindrome(int x) {
+
+//         String s = String.valueOf(x);
+
+//         int i = 0;
+//         int j = s.length() - 1;
+
+//         while (i < j) {
+//             if (s.charAt(i) != s.charAt(j)) {
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+
+//         return true;
+//     }
+// }
