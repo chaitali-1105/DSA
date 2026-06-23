bc04c1b7a1fd1d67418b7027c15774183eef6813
chaitali-1105 <chaitali.work11@gmail.com>
2026-06-23 11:01:33 +0530

    Time: 3 ms (68.31%), Space: 45.4 MB (50.21%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,11 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for( int i=1;i<nums.length;i+=3){
-            if(nums[i]!=nums[i-1]){
-                return nums[i-1];
+   
+      int ans=0;
+      for(int i=0;i<32;i++){
+           int count =0;
+        for(int num:nums){
+            if((num & (1<<i)) !=0){
+                count++;
             }
         }
-        return nums[nums.length-1];
+        if(count%3!=0){
+            ans=ans|(1<<i);
+        }
+      }
+      return ans;
     }
 }
