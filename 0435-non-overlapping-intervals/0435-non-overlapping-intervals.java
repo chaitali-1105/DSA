42dd13cc62dce63b1455655bb14045d16a36d4de
chaitali-1105 <chaitali.work11@gmail.com>
2026-06-29 21:02:15 +0530

    Time: 53 ms (16.12%), Space: 115.5 MB (93.99%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -4,22 +4,22 @@ class Solution {
             int start;
             int end;
 
-        pair(int start,int end){
-            this.start=start;
-            this.end=end;
-        }
-        @Override 
-        public int compareTo(pair other){
-            return Integer.compare(this.end,other.end);
-        }
-       } 
+            pair(int start,int end){
+                this.start=start;
+                this.end=end;
+            }
+            @Override 
+            public int compareTo(pair other){
+                return Integer.compare(this.end,other.end);
+                 }
+        } 
        int n=intervals.length;
        pair[]  mat= new pair[n];
        for(int i=0;i<n;i++){
             mat[i]=new pair(intervals[i][0],intervals[i][1]);
        }
        Arrays.sort(mat);
-       int remove=0;
+        int remove=0;
         int prevend=mat[0].end;
          for (int i = 1; i < n; i++) {
 
