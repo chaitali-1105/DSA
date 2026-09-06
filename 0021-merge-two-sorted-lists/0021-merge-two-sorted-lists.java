2929b742035bce6a84b3d4bfd50748d4402864f1
chaitali-1105 <chaitali.work11@gmail.com>
2026-09-06 06:51:41 +0530

    Time: 0 ms (100%), Space: 44.6 MB (6.94%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -14,7 +14,7 @@ class Solution {
             return list1==null?list2:list1;
         }
 
-        if(list1.val<list2.val){
+        if(list1.val<=list2.val){
             list1.next=mergeTwoLists(list1.next,list2);
             return list1;
         }
